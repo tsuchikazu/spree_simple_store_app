d5f8fdc7d23a7fb2a4459d8e00fbde2b1a886180
tsuchikazu <huneyhunt77@gmail.com>
2013-02-01 00:41:40 +0900

    Generate with: $ rails new spree_simple_store_app -T -d mysql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SpreeSimpleStoreApp::Application.config.secret_token = 'bab3f6436677d4478a0ad7aafe44b6e7eb50289d5c0ec29e97ed8c73c8faa14d6ff116cd2e1a3f67378def1e4d9f8b9dd4ea502ab8b9a18b01d8ee2eb7128cf0'
